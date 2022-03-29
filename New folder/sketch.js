bf5a00f830b8afe0b2b11526cd0da025752db2ea
BrigJohnson <97643406+BrigJohnson@users.noreply.github.com>
2022-03-28 18:46:35 -0700

    Add files via upload

---
+++ New folder/sketch.js
@@ -0,0 +1,35 @@
+
+function setup() {
+	createCanvas(800, 600);
+}
+
+function draw() {
+	background(0);
+stroke(0);
+strokeWeight(4);
+rect( 270, 250, 150);
+if (mouseX < 400) {
+	fill(255, 0, 200);
+rect (420, 290, 10, 150);
+rect (260, 290, 10, 150);
+rect (300, 400, 20, 120);
+rect (370, 400, 20, 120);
+ellipse(345, 195, 120, 120);
+rect(365, 195, 15, 1)
+rect(315, 195, 15, 1)
+}
+
+
+
+
+
+if (mouseX > 400) {
+	fill(255, 50, 0);
+rect (420, 290, 150, 10);
+rect (260, 290, 150, 10);
+rect (300, 400, 20, 120);
+rect (370, 400, 120, 20);
+ellipse(345, 195, 120, 120);
+ellipse(370, 195, 20, 20);
+ellipse(325, 195, 20, 20);
+}
