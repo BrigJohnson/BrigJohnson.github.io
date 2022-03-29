40b4184808e16785d0b06e309c0ecc87b4785363
BrigJohnson <97643406+BrigJohnson@users.noreply.github.com>
2022-03-28 20:39:48 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+function setup() {
+	createCanvas(1400,750);
+}
+function draw() {
+	background(0);
+
+
+animateDM(mouseX, mouseY);
+
+}
+
+function animateDM(x, y) {
+	push();
+	translate(x, y);
+
+
+	if ((frameCount / 15) % 2 < 1) {
+    drawDM1();
+	}
+   else {
+		drawDM2();
+	}
+	pop();
+}
+
+function drawDM1() {
+  fill(255, 0, 200);
+rect( 270, 250, 150);
+  rect (420, 290, 10, 150);
+  rect (260, 290, 10, 150);
+  rect (300, 400, 20, 120);
+  rect (370, 400, 20, 120);
+  ellipse(345, 195, 120, 120);
+  rect(365, 195, 15, 1)
+  rect(315, 195, 15, 1)
+  }
+
+function drawDM2() {
+  fill(255, 50, 0);
+
+  rect( 270, 250, 150);
+rect (420, 290, 150, 10);
+rect (260, 290, 150, 10);
+rect (300, 400, 20, 120);
+rect (370, 400, 120, 20);
+ellipse(345, 195, 120, 120);
+ellipse(370, 195, 20, 20);
+ellipse(325, 195, 20, 20);
+}
