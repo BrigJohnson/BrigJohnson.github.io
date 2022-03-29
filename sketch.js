6632fd1f51faeec136d6be3e4b328433f700fd71
BrigJohnson <97643406+BrigJohnson@users.noreply.github.com>
2022-03-28 17:44:08 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,61 +1,49 @@
-let kylie;
-let curry;
-let travis;
-let bitcoin;
-let tesla;
-let kylie1;
-let curry1;
-let tesla1;
-let bitcoin1;
-let travis1;
-let krusty;
-
-function preload() {
-
-
-
-
-
-
-
-}
 function setup() {
+	createCanvas(1400,750);
+}
+function draw() {
+	background(0);
 
 
-
+animateDM(mouseX, mouseY);
 
 }
 
-function draw() {
+function animateDM(x, y) {
+	push();
+	translate(x, y);
 
 
- kylie.size(400,600)
-kylie.position(770,220);
-curry.position(225, 220);
-  curry.size(200,300);
-tesla.position(125, 675);
-tesla.size(400, 300);
-bitcoin.position(1400, 250);
-bitcoin.size(300, 300);
-travis.position(1360, 675);
-travis.size (400, 300);
-kylie1.style ('font-size', '18px');
-kylie1.position (785,830);
-kylie1.size (400, 200);
-curry1.style ('font-size', '18px');
-curry1.position (125, 525);
-curry1.size (400,100);
-tesla1.style ('font-size', '18px');
-tesla1.position(125, 970);
-tesla1.size(420, 50);
-bitcoin1.style ('fotn-size', '18px')
-bitcoin1.position(1360, 555);
-bitcoin1.size(420, 100);
-travis1.style('font-size', '18px')
-travis1.position(1360, 970);
-travis1.size(420,50);
-krusty.size (100,100);
-krusty.position (30,50);
+	if ((frameCount / 15) % 2 < 1) {
+    drawDM1();
+	}
+   else {
+		drawDM2();
+	}
+	pop();
+}
 
+function drawDM1() {
+  fill(255, 0, 200);
+rect( 270, 250, 150);
+  rect (420, 290, 10, 150);
+  rect (260, 290, 10, 150);
+  rect (300, 400, 20, 120);
+  rect (370, 400, 20, 120);
+  ellipse(345, 195, 120, 120);
+  rect(365, 195, 15, 1)
+  rect(315, 195, 15, 1)
+  }
 
+function drawDM2() {
+  fill(255, 50, 0);
+
+  rect( 270, 250, 150);
+rect (420, 290, 150, 10);
+rect (260, 290, 150, 10);
+rect (300, 400, 20, 120);
+rect (370, 400, 120, 20);
+ellipse(345, 195, 120, 120);
+ellipse(370, 195, 20, 20);
+ellipse(325, 195, 20, 20);
 }
